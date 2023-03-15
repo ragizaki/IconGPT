772b634b2b75fd6441580bb10ae13a418bf94f26
Zaki Machfj <zakimachfj@gmail.com>
2023-03-15 01:02:59 -0400

    update images

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,11 @@ const config = {
         hostname: "res.cloudinary.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
     ],
   },
   /**
