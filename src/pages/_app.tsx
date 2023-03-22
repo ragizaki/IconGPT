c87008ce700b5fb03a3023b552a499a75751f9f7
Zaki Machfj <zakimachfj@gmail.com>
2023-03-22 01:19:18 -0400

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,23 @@ import { SessionProvider } from "next-auth/react";
 import Layout from "@/components/Layout";
 
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
   );
 };
 
