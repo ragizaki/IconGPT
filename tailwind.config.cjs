a62c433f0f819a90a4101084341829ec47b8786d
Zaki Machfj <zakimachfj@gmail.com>
2023-03-23 20:42:08 -0400

    add xs breakpoint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "400px",
+      },
+    },
   },
   plugins: [],
 };
